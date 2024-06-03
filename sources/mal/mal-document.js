07083ee35205f66ba29f4537a3ca3bb2b6578329
ch4nny <79906474+ch4nny@users.noreply.github.com>
2024-06-03 18:42:38 +0100

    Update mal-document.js

--- sources/mal/mal-document.js
+++ sources/mal/mal-document.js
@@ -1,1 +1,19 @@
+// MAL manga document (1.0)
+// https://github.com/ch4nny/collections-mal
 
+app.api.tmdb.api_key = "YOUR API KEY";
+
+let manga = app.api.mal.getManga(app.params.id);
+
+if(manga == undefined) {
+    app.fail();
+}
+
+let builder = app.document.builder();
+
+builder.setString(movie.title, "title");
+builder.setImage(movie.main_picture(), "cover");
+builder.setString(movie.synopsis, "synopsis");
+builder.setDate(movie.releaseDate, "release-date");
+
+app.result(builder);
