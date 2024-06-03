b6797f0f3ea61d35b40148c9b6c463df5137e4f0
ch4nny <79906474+ch4nny@users.noreply.github.com>
2024-06-03 18:32:48 +0100

    Update mal-search.js

--- sources/mal/mal-search.js
+++ sources/mal/mal-search.js
@@ -1,1 +1,7 @@
+// MAL movies search (1.0)
+// https://github.com/ch4nny/collections-mal
 
+app.api.mal.api_key = "YOUR API KEY";
+
+let results = app.api.mal.searchManga(app.query);
+app.result(results);
